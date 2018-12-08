22e4504a59985a20aee28a5378371199bd6e8f71
Dominik Skála <DominikSkala@seznam.cz>
2018-12-08 17:50:16 +0100

    readme and improved generation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys, getopt
+import sys
+import getopt
 import pandas
 from sms_prices import SMS
 
@@ -17,9 +18,11 @@ def main(argv):
 
     sum = {'CZK': 0, 'EUR': 0}
     sum_sms = {'CZK': 0, 'EUR': 0}
-    credit_sum = {'CZK': 0, 'EUR': 0}
 
     currency_course = {'CZK': 1, 'EUR': 25}
+    credit_course = {'CZK': 0.7, 'EUR': 0.647}
+    charge = 5 # charge for withdrawal in percents
+    charge_percents = 1 - (5/100)
 
     if len(file):
         lines = pandas.read_csv(file, usecols=['currency', 'phone_operator', 'amount'])
@@ -31,17 +34,21 @@ def main(argv):
                 real_price = SMS[currency][operator][price]
                 sum_sms[currency] += price
                 sum[currency] += real_price
-                credit_sum[currency] += price*0.7
 
-    for currency in currency_course:
-        print('======================================================================================')
-        print('MENA: '+currency)
-        print('======================================================================================')
-        print('Celkova cena z SMS pro kontrolu (porovnat se sumou z MySQL): ' + str(sum_sms[currency]))
-        print('Realna cena z SMS: ' + str(sum[currency] * currency_course[currency]))
-        print('Realna cena po odecteni 5% poplatku: ' + str(sum[currency] * currency_course[currency] * 0.95))
-        print('Suma kreditu pro kontrolu (porovnat se sumou z MySQL): ' + str(credit_sum[currency]))
-        print('======================================================================================')
+        for currency in currency_course:
+            if sum_sms[currency]:
+                print('======================================================================================')
+                print('MENA: '+currency)
+                print('======================================================================================')
+                print('Celkova cena z SMS pro kontrolu (porovnat se sumou z MySQL): ' + str(sum_sms[currency]))
+                print('Realna cena z SMS: ' + str(sum[currency] * currency_course[currency]))
+                print('Realna cena po odecteni 5% poplatku: ' + str(sum[currency] * currency_course[currency] * charge_percents))
+                print('Suma kreditu pro kontrolu (porovnat se sumou z MySQL): ' + str(sum_sms[currency] * credit_course[currency]))
+                print('======================================================================================')
+
+    else:
+        print('File not specified')
+        exit(1)
 
 
 if __name__ == "__main__":
---
+++ readme.md
@@ -0,0 +1,9 @@
+### Automatic SMS calculator
+
+Script parses csv file line and outputs the proper amount that the user should get lowered by 5 % charge for withdrawal.
+
+The CSV file must be delimited with commas (',').
+
+The script takes only one parameter and that is -f, --file parameter. Using this parameter, the input file is specified. 
+
+The output is always performed to stdout. 
