0674d0fca4fcdd3f4f8e8a8af744c210d1f48fd2
Dominik Skála <DominikSkala@seznam.cz>
2018-12-08 17:40:30 +0100

    cleanup of sms prices and unifying access to output data

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys, getopt
 import pandas
 from sms_prices import SMS
 
+
 def main(argv):
 
     file = ''
@@ -14,9 +15,11 @@ def main(argv):
         if opt in ("-f", "--f"):
             file = arg
 
-    SUM = {'CZK': 0, 'EUR': 0}
-    SUMSMS = {'CZK': 0, 'EUR': 0}
-    CREDITSUM = {'CZK': 0, 'EUR': 0}
+    sum = {'CZK': 0, 'EUR': 0}
+    sum_sms = {'CZK': 0, 'EUR': 0}
+    credit_sum = {'CZK': 0, 'EUR': 0}
+
+    currency_course = {'CZK': 1, 'EUR': 25}
 
     if len(file):
         lines = pandas.read_csv(file, usecols=['currency', 'phone_operator', 'amount'])
@@ -26,14 +29,19 @@ def main(argv):
             price = lines.iloc[i].amount
             if currency == 'CZK':
                 real_price = SMS[currency][operator][price]
-                SUMSMS[currency] += price
-                SUM[currency] += real_price
-                CREDITSUM[currency] += price*0.7
+                sum_sms[currency] += price
+                sum[currency] += real_price
+                credit_sum[currency] += price*0.7
 
-    print('Celkova cena z SMS pro kontrolu (porovnat se sumou z MySQL): ' + str(SUMSMS['CZK']))
-    print('Realna cena z SMS: ' + str(SUM['CZK']))
-    print('Realna cena po odecteni 5% poplatku: ' + str(SUM['CZK']*0.95))
-    print('Suma kreditu pro kontrolu (porovnat se sumou z MySQL): ' + str(CREDITSUM['CZK']))
+    for currency in currency_course:
+        print('======================================================================================')
+        print('MENA: '+currency)
+        print('======================================================================================')
+        print('Celkova cena z SMS pro kontrolu (porovnat se sumou z MySQL): ' + str(sum_sms[currency]))
+        print('Realna cena z SMS: ' + str(sum[currency] * currency_course[currency]))
+        print('Realna cena po odecteni 5% poplatku: ' + str(sum[currency] * currency_course[currency] * 0.95))
+        print('Suma kreditu pro kontrolu (porovnat se sumou z MySQL): ' + str(credit_sum[currency]))
+        print('======================================================================================')
 
 
 if __name__ == "__main__":
